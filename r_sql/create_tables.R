51d89bd06a7ec9a5b21ca4ffd4454cf6e06e6ccc
Bastien Winant <babw1@student.london.ac.uk>
2022-09-14 00:27:07 +0200

    add comments to notebooks

--- r_sql/create_tables.R
+++ r_sql/create_tables.R
@@ -1,37 +1,20 @@
 library(DBI)
 
 initialise_db <- function() {
+  working_directory = getwd()
+  setwd('..')
+  
   db_filename <- "airline2.db"
   
-  # change working directory
-  current_wd = getwd()
-  setwd('../..')
-  
-  if (file.exists(db_filename)) {
+  # remove the db file if it exists
+  if (file.exists(db_filename))
     file.remove(db_filename)
-  }
   
+  # create database and open connection
   conn <- dbConnect(RSQLite::SQLite(), db_filename)
   
-  # restore working directory
-  setwd(current_wd)
+  # restore the initial working directory
+  setwd(working_directory)
   
-  return(conn)
-}
-
-
-create_tables <- function(conn) {
-  current_wd = getwd()
-  setwd('../../dataverse_files')
-  
-  airports <- read.csv("airports.csv", header = TRUE)
-  carriers <- read.csv("carriers.csv", header = TRUE)
-  planes <- read.csv("plane-data.csv", header = TRUE)
-  
-  dbWriteTable(conn, "airports", airports)
-  dbWriteTable(conn, "carriers", carriers)
-  dbWriteTable(conn, "planes", planes)
-  
-  # restore working directory
-  setwd(current_wd)
+  return (conn)
 }
